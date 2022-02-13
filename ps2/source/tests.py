6680dd027bb5583184215b2a2fe554cfdddba342
Miroslav Shubernetskiy <miroslav@miki725.com>
2022-02-13 18:11:29 -0500

    matching existing grading structure for problem 1

--- ps2/source/tests.py
+++ ps2/source/tests.py
@@ -16,29 +16,26 @@ def take_in_chunks(data: str, n) -> typing.Iterator[str]:
         next_chunk = list(islice(idata, n))
 
 
-@weight(name="problem 1", worth=30)
-def test_problem_1(score: Score, problem: Problem):
-    for i, (input, output, reference) in enumerate(
-        zip_longest(problem.input["new_trades"], problem.output, problem.reference)
-    ):
-        # replaced target trades with one of possible trades
-        # but does not guarantee its replaced with best trade
-        with score.test(msg=f"part {i} (one of possible trades)", i=i, weight=7):
-            valid_mappings = {
-                target.lower(): list(problem.artifacts["possible"].values())
-                for target in problem.artifacts["targets"].values()
-            }
-            for input_trade, output_trade in zip_longest(
-                take_in_chunks(input, 32), take_in_chunks(output, 32)
-            ):
-                if input_trade.lower() in problem.artifacts["targets"].values():
-                    assert output_trade.lower() in valid_mappings[input_trade.lower()]
-                else:
-                    assert output_trade.lower() == input_trade.lower()
+@weight(name="problem 1", worth=20)
+def test_problem_1_acceptable_trade(problem: Problem):
+    for input, output in zip_longest(problem.input["new_trades"], problem.output):
+        valid_mappings = {
+            target.lower(): list(problem.artifacts["possible"].values())
+            for target in problem.artifacts["targets"].values()
+        }
+        for input_trade, output_trade in zip_longest(
+            take_in_chunks(input, 32), take_in_chunks(output, 32)
+        ):
+            if input_trade.lower() in problem.artifacts["targets"].values():
+                assert output_trade.lower() in valid_mappings[input_trade.lower()]
+            else:
+                assert output_trade.lower() == input_trade.lower()
 
-        # replaced with best possible trade
-        with score.test(msg=f"part {i} (best trade)", i=i, weight=8):
-            assert output.lower() == reference.lower()
+
+@weight(name="problem 1", worth=10)
+def test_problem_1_best_trade(problem: Problem):
+    for output, reference in zip_longest(problem.output, problem.reference):
+        assert output.lower() == reference.lower()
 
 
 @weight(name="problem 2", worth=30)
